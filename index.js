0d3df8c4262ab1e0e0feec815fa9a4898350207d
jonathanpenaloa <jona_pena_91@outlook.com>
2023-04-02 14:31:32 -0700

    done

--- index.js
+++ index.js
@@ -81,7 +81,7 @@ const returnIsChase = (name) => {
 console.log("#7", returnIsChase('ChaSe'));
 
 // 8. Make a variable called class that is an array ["Sally", "Sam", "Frodo", "Gandalf", "Eragorn", "Gimley"]
-let classNames = ["Sally", "Sam", "Frodo", "Gandalf", "Eragorn", "Gimley", "gs", "nd", "jsa", "fhd", "ahd"];
+let classNames = ["Sally", "Sam", "Frodo", "Gandalf", "Eragorn", "Gimley"];
 // Write a function called returnClassIsTooSmall that will return true if the students parameter has less than 10 students
 const returnClasIsTooSmall = (classeSize) => {
     return classeSize.length < 10 ? true : "this is a large class";
@@ -93,12 +93,59 @@ console.log("#8", returnClasIsTooSmall(classNames));
 
 
 // 9. Make a variable called combineStrings that will take stringOne parameter and stringTwo parameter and return them combined
+const combineString = (stringOne, stringTwo) => {
+    let combined = `${stringOne}${stringTwo}`;
+    return combined;
+}
+
 // call this function 3 times with 6 strings.
+console.log("#9" ,combineString("hello", "Welcome"));
+console.log("#9" ,combineString("hello", "Welcome"));
+console.log("#9" ,combineString("hello", "Welcome"));
+console.log("#9" ,combineString("hello", "Welcome"));
+
+
 // 10. Write a function called returnDataType that will return the datatype of the data parameter
+const returnDataType = (param) => {
+    return typeof param;
+}
+
+console.log("#10 type of paramiter is: ", returnDataType(2));
+
 
 // 11. Write a function called addStudentToClass that will push the student parameter to the class array from problem number 8
+const addStudentToClass = (newStudent) => {
+     classNames.push(newStudent);
+}
+
+addStudentToClass("Will");
+// addStudentToClass("Joe");
+// addStudentToClass("Mike");
+// addStudentToClass("Lisa");
+// addStudentToClass("Maria");
+
+// console.log("#11", classNames); 
 
 // Call this function 2 times
 // 12. VERY OPTIONAL - Make a variable called extraStudents. This should be an array with about 10 string names. 
+let extraStudents = ["Tiffany", "Erika", "Nick", "Chase", "Edgar", "Jonathan", "Jay", "Mark", "Tony"];
 
-// Write a function called fillClass that will loop until the class array has >= 15 students. Every loop one student should be put into the class array and taken out of the extraStudents array
+// Write a function called fillClass that will loop until the class array has >= 15 students. 
+// Every loop one student should be put into the class array and taken out of the extraStudents array
+const fillClass = (extraStudents) => {
+    let limetClassZise;
+    for(let i=0; classNames.length < 10; i++) {
+        if(classNames.length < 10) {
+            extraStudents.forEach(name => {
+                limetClassZise = classNames.push(name);
+            });
+            return limetClassZise;
+        }
+    }
+}
+
+// combined = classNames.concat(extraStudents);
+    // done = combined.filter(size => size.length > 5);
+    // console.log(done);
+
+console.log(fillClass(extraStudents));
